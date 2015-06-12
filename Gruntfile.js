d0b31954517f8faf90dd4185b1b46a9ea1eda612
Nathan Nelson <osidenate@gmail.com>
2015-06-12 16:48:07 -0700

    Updated gruntfile to use grunt-gh-pages for deployments

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,40 +3,38 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        'gh-pages': {
+            'deploy': {
+                options: {
+                    branch: 'gh-pages',
+                    base: '_site'
+                },
+                src: ['**']
+            }
+        },
         shell: {
             jekyll: {
                 command: 'jekyll build'
             }
         },
-        copy: {
-            package: {
-                src: '_site/*',
-                dest: './'
-            }
-        },
         clean: {
-            package: [
-                '_includes',
-                '_layouts',
-                '_posts',
-                '_sass',
-                'css',
-                '_config.yml',
-                'about.md',
-                'feed.xml',
-                'LICENSE',
-                'README.md'
+            site: [
+                '_site/node_modules',
+                '_site/Gruntfile.js',
+                '_site/LICENSE',
+                '_site/package.json',
+                '_site/README.md'
             ]
         }
     });
 
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
-    grunt.registerTask('package', [
+    grunt.registerTask('deploy', [
         'shell:jekyll',
-        'copy:package',
-        'clean:package'
+        'clean:site',
+        'gh-pages:deploy'
     ]);
 };
