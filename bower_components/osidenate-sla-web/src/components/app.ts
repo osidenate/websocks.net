f77e7f8353845f72145e07027d3306f8d044888f
Nathan Nelson <osidenate@gmail.com>
2015-07-27 01:06:36 -0700

    Updates

--- bower_components/osidenate-sla-web/src/components/app.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-
-module SLAClient {
-    angular.module('sla', ['firebase'])
-        .constant('firebaseUrl', 'https://sla-monitor-dev.firebaseio.com/');
-}
